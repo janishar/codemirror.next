6e4fa982032b67d447e5b426f9d5b4cc47b65b33
Janishar Ali <janishar.ali@gmail.com>
2026-01-08 15:02:47 +0530

    add all languge support

--- local/editor.js
+++ local/editor.js
@@ -1,10 +1,21 @@
 import { EditorView, basicSetup } from "codemirror";
-import { javascript, esLint, snippets } from "@codemirror/lang-javascript";
 import { oneDark } from "@codemirror/theme-one-dark";
 import Linter from "eslint4b-prebuilt";
 import { linter } from "@codemirror/lint";
 import { snippetCompletion } from "@codemirror/autocomplete";
 import * as commands from "@codemirror/commands";
+import { javascript, esLint } from "@codemirror/lang-javascript";
+import { python } from "@codemirror/lang-python";
+import { cpp } from "@codemirror/lang-cpp";
+import { java } from "@codemirror/lang-java";
+import { rust } from "@codemirror/lang-rust";
+import { sql } from "@codemirror/lang-sql";
+import { html } from "@codemirror/lang-html";
+import { css } from "@codemirror/lang-css";
+import { go } from "@codemirror/lang-go";
+import { json } from "@codemirror/lang-json";
+import { markdown } from "@codemirror/lang-markdown";
+import { php } from "@codemirror/lang-php";
 
 const extensions = [
   basicSetup,
@@ -25,12 +36,26 @@ const extensions = [
   ),
 ];
 
+const languages = {
+  javascript: javascript,
+  python: python,
+	cpp: cpp,
+	java: java,
+	rust: rust,
+	sql: sql,
+	html: html,
+	css: css,
+	go: go,
+	json: json,
+	markdown: markdown,
+	php: php,
+};
+
 const CodeEditor = {
   EditorView: EditorView,
   extensions: extensions,
-  snippets: snippets,
   commands: commands,
   snippetCompletion: snippetCompletion,
-  javascript: javascript,
+  languages: languages,
 };
 window.CodeEditor = CodeEditor;
